bbae876bc8d468bb0fe56f9d7704421b05d1e412
Christine <2739361+christine-aqui@users.noreply.github.com>
2018-07-04 23:21:01 -0400

    added add note function in cli

--- notes.js
+++ notes.js
@@ -1,7 +1,30 @@
 console.log('Starting notes.js file');
+const fs = require('fs');
+
 
 let addNote = (title, body) => {
-  console.log('Adding notes: ', title, body);
+  let notes = [];
+  let note = {
+    title: title,
+    body: body
+  }
+  //
+  // try this block of code and if notes-data is missing catch error without breaking code.
+  try {
+    let notesString = fs.readFileSync('notes-data.json'); // reads the file to see if any data present and store it
+    notes = JSON.parse(notesString); // this allows you to add a new notes without losing any exisiting ones.
+  } catch (e) {}
+  //
+  let duplicateNotes = notes.filter((note) => note.title === title);
+  //
+  if (duplicateNotes.length === 0) {
+    notes.push(note);
+    fs.writeFileSync('notes-data.json', JSON.stringify(notes));
+    console.log(`Adding notes: \nTitle: ${title} \nBody:${body}`);
+// will add note if it not a duplicate.
+  } else {
+    console.log("Duplicate Notes")
+  }
 };
 //
 let getAll = () => {
@@ -16,7 +39,7 @@ let removeNote = (title) => {
   console.log('Removing note: ', title);
 }
 
-module.exports  = {
+module.exports = {
   addNote,
   getAll,
   getNote,
